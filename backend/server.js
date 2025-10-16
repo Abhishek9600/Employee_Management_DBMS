7d901ec1dc83c7d8fb6b8101cd18ed048dec0cc0
Abhishek Chaubey <abhishekchaubey501@gmail.com>
2025-10-17 00:28:13 +0530

    minor bug

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://employeemanagementdbmsminor.netlify.app/'],
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://employeemanagementdbmsminor.netlify.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
